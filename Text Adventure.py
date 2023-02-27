a15d8b31a814425faa0093eb3ad3939d8785e539
Kyle Li <kyleseanli@gmail.com>
2023-02-27 04:33:10 -0500

    implemented map generation & stable seeds

--- Text Adventure.py
+++ Text Adventure.py
@@ -1,6 +1,6 @@
 import random
 
-class GameObject:
+class gameMethod:
     verb_dict = {
         "examine": "Examine a creature in the room. Use by typing examine [target].",
         "hit": "Hit a creature in the room. Use by typing hit [target].",
@@ -14,8 +14,8 @@ class GameObject:
     def get_input():
         command = input(": ").split()
         verb_word = command[0]
-        if verb_word in GameObject.verb_dict:
-            verb = getattr(GameObject, verb_word)
+        if verb_word in gameMethod.verb_dict:
+            verb = getattr(gameMethod, verb_word)
         else:
             print(f"Unknown verb {verb_word}")
             print("For a list of commands type \"help\"")
@@ -53,9 +53,9 @@ class GameObject:
     @staticmethod
     def help(noun):
         if noun == "nothing":
-            return list(GameObject.verb_dict.keys())
-        elif noun in GameObject.verb_dict:
-            return GameObject.verb_dict[noun]
+            return list(gameMethod.verb_dict.keys())
+        elif noun in gameMethod.verb_dict:
+            return gameMethod.verb_dict[noun]
         else:
             return f"Unknown command {noun}"
     
@@ -82,11 +82,77 @@ class GameObject:
     def status(noun):
         return f"Your name is {character.name}, you have {character.health}/{character.max_health} HP.\nInventory: {character.inventory}"
 
+class Dungeon:
+    def __init__(self, num_rooms, world_seed=None):
+        self.rooms = []
+        self.generate(num_rooms, world_seed)
+
+    def generate(self, num_rooms, world_seed=None):
+        if world_seed is None:
+            world_seed = random.Random()
+        starting_room = Entrance()
+        self.rooms.append(starting_room)
+        unconnected_rooms = [starting_room]
+
+        for i in range(num_rooms - 1):
+            new_room = world_seed.choice([Hallway, Prison, TreasureRoom])()
+            connected = False
+            while not connected:
+                parent_room = world_seed.choice(unconnected_rooms)
+                if len(parent_room.adjacent_rooms) < 3:
+                    # try to connect room to parent room
+                    direction = world_seed.choice(["north", "south", "east", "west"])
+                    connected = new_room.connect_to(parent_room, direction)
+                    if connected:
+                        unconnected_rooms.append(new_room)
+                        self.rooms.append(new_room)
+
+        for room in self.rooms:
+            print(room.name, [r.name for r in room.adjacent_rooms])
+
 class Room:
-    def __init__(self, name):
+    num_rooms = {}  # class variable to keep track of number of rooms of each subclass
+
+    def __init__(self, name, x=0, y=0):
         self.name = name
         self.creatures = []
         self.adjacent_rooms = []
+        if name not in Room.num_rooms:
+            Room.num_rooms[name] = 1
+        else:
+            Room.num_rooms[name] += 1
+        self.name += f" {Room.num_rooms[name]}"
+        self.x = x
+        self.y = y
+
+    def connect_to(self, other, direction):
+        if len(self.adjacent_rooms) >= 3 or len(other.adjacent_rooms) >= 3:
+            return False
+        if direction == "north":
+            if other.y == 0 or self.y == other.y + 1:
+                self.adjacent_rooms.append(other)
+                other.adjacent_rooms.append(self)
+                self.y = other.y - 1
+                return True
+        elif direction == "south":
+            if other.y == 0 or self.y == other.y - 1:
+                self.adjacent_rooms.append(other)
+                other.adjacent_rooms.append(self)
+                self.y = other.y + 1
+                return True
+        elif direction == "east":
+            if other.x == 0 or self.x == other.x - 1:
+                self.adjacent_rooms.append(other)
+                other.adjacent_rooms.append(self)
+                self.x = other.x + 1
+                return True
+        elif direction == "west":
+            if other.x == 0 or self.x == other.x + 1:
+                self.adjacent_rooms.append(other)
+                other.adjacent_rooms.append(self)
+                self.x = other.x - 1
+                return True
+        return False
 
     def add_creature(self, creature):
         self.creatures.append(creature)
@@ -101,31 +167,22 @@ class Room:
             return "There is nothing in this room"
         return f"In this room is {[str(i) for i in self.creatures]}."
 
-class Creature:
-    def __init__(self, name, class_name, desc, health, damage_multiplier, damage_range):
-        self.name = name
-        self.class_name = class_name
-        self.desc = desc
-        self.max_health = health
-        self.health = health
-        self.damage_multiplier = damage_multiplier
-        self.damage_range = damage_range
-    
-    def attack(self, target):
-        damage_randomized = self.damage_multiplier * random.randrange(*self.damage_range)
-        target.health -= damage_randomized
-        print(f"The {self.name} hit you for {damage_randomized} damage!")
+class Entrance(Room):
+    def __init__(self):
+        super().__init__("entrance")
 
-    def __str__(self):
-        return self.name
+class Hallway(Room):
+    def __init__(self):
+        super().__init__("hallway")
 
-    def get_desc(self):
-        if self.health >= self.max_health:
-            return self.name + "\n" + self.desc
-        else:
-            health_line = f"{self.health}/{self.max_health} HP"
-            return self.name + "\n" + self.desc + "\n" + health_line
-    
+class Prison(Room):
+    def __init__(self):
+        super().__init__("prison")
+
+class TreasureRoom(Room):
+    def __init__(self):
+        super().__init__("treasure room")
+
 class Player:
     def __init__(self, name, starting_room, health=25, damage_multiplier=1, damage_range=(1,4), inventory=["Sword"]):
         self.name = name
@@ -157,6 +214,31 @@ class Player:
             heal_amount = self.max_health - self.health
         self.health += heal_amount
         print(f"You have healed for {heal_amount} HP.")
+
+class Creature:
+    def __init__(self, name, class_name, desc, health, damage_multiplier, damage_range):
+        self.name = name
+        self.class_name = class_name
+        self.desc = desc
+        self.max_health = health
+        self.health = health
+        self.damage_multiplier = damage_multiplier
+        self.damage_range = damage_range
+    
+    def attack(self, target):
+        damage_randomized = self.damage_multiplier * random.randrange(*self.damage_range)
+        target.health -= damage_randomized
+        print(f"The {self.name} hit you for {damage_randomized} damage!")
+
+    def __str__(self):
+        return self.name
+
+    def get_desc(self):
+        if self.health >= self.max_health:
+            return self.name + "\n" + self.desc
+        else:
+            health_line = f"{self.health}/{self.max_health} HP"
+            return self.name + "\n" + self.desc + "\n" + health_line
 
 class Goblin(Creature):
     possible_names = ["green goblin", "angry goblin", "sticky goblin", "bloody goblin", "warted goblin", "goblin"]
@@ -218,31 +300,12 @@ class Dragon(Creature):
             (6,10)
         )
 
-entrance = Room("entrance")
-goblin1 = Goblin()
-entrance.add_creature(goblin1)
-
-hallway = Room("hallway")
-slime1 = Slime()
-hallway.add_creature(slime1)
-
-dungeon = Room("dungeon")
-troll1 = Troll()
-dungeon.add_creature(troll1)
-
-treasure_room = Room("treasure_room")
-dragon1 = Dragon()
-treasure_room.add_creature(dragon1)
-
-entrance.adjacent_rooms.append(hallway)
-hallway.adjacent_rooms.append(entrance)
-hallway.adjacent_rooms.append(dungeon)
-hallway.adjacent_rooms.append(treasure_room)
-dungeon.adjacent_rooms.append(hallway)
-treasure_room.adjacent_rooms.append(hallway)
+seed = input("Enter a seed: ")
+world_seed = random.Random(seed)
+dungeon = Dungeon(5, world_seed)
 
 name = input("What is your name? ")
-character = Player(name, entrance)
+character = Player(name, dungeon.rooms[0])
 
 while character.health > 0:
     if not character.current_room.creatures:
@@ -253,7 +316,7 @@ while character.health > 0:
         character.move_to_room(next_room)
         character.current_room.contents
     else:
-        GameObject.get_input()
+        gameMethod.get_input()
 else:
     print("You have 0 HP.")
     print("Game Over!")
